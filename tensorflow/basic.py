e5807ab0fb6476b94cc5c6797e1271a2fa86842e
krishnakalyan3 <krishnakalyan3@gmail.com>
2016-12-08 21:30:47 +0100

    basic

---
+++ tensorflow/basic.py
@@ -0,0 +1,27 @@
+#/usr/bin/env python
+
+import tensorflow as tf
+
+a = tf.constant(2)
+b = tf.constant(3)
+
+sess = tf.Session()
+print sess.run(a+b)
+
+a = tf.placeholder(tf.int16)
+b = tf.placeholder(tf.int16)
+
+a = tf.constant(2)
+b = tf.constant(3)
+
+add = tf.add(a,b)
+mul = tf.mul(a,b)
+
+print sess.run(add)
+print sess.run(mul)
+
+matrix1 = tf.constant([[3.,3.]])
+matrix2 = tf.constant([[2.],[2.]])
+product = tf.matmul(matrix1,matrix2)
+
+print sess.run(product)
