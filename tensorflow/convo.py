b4b74a1ab42b548ca6e26985838b1304e5e6e8e7
krishnakalyan3 <krishnakalyan3@gmail.com>
2016-12-27 10:37:28 +0100

    convo

--- tensorflow/convo.py
+++ tensorflow/convo.py
@@ -6,7 +6,7 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist =  input_data.read_data_sets('MNIST_data',one_hot = True)
 
 learning_rate = 0.001
-training_iters = 200000
+training_iters = 20000
 batch_size = 128
 display_step = 10
 
@@ -30,7 +30,7 @@ def conv_net(x, weights, biases, dropout):
 	x = tf.reshape(x, shape=[-1,28,28,1])
 	conv1 = conv2d(x, weights['wc1'], biases['bc1'])
 	conv1 = maxpool2d(conv1,k=2)
-	conv2 = conv2d(conv1, weight['wc2'], biases['bc2'])
+	conv2 = conv2d(conv1, weights['wc2'], biases['bc2'])
 	conv2 = maxpool2d(conv2, k=2)
 	fc1 = tf.reshape(conv2, [-1, weights['wd1'].get_shape().as_list()[0]])
 	fc1 = tf.add(tf.matmul(fc1, weights['wd1']), biases['bd1'])
@@ -39,7 +39,7 @@ def conv_net(x, weights, biases, dropout):
 	out = tf.add(tf.matmul(fc1, weights['out']), biases['out'])
 	return out	
 
-weigths = {
+weights = {
 	'wc1': tf.Variable(tf.random_normal([5,5,1,32])),
 	'wc2': tf.Variable(tf.random_normal([5,5,32,64])),
 	'wd1': tf.Variable(tf.random_normal([7*7*64, 1024])),
@@ -49,6 +49,28 @@ weigths = {
 biases = {
 	'bc1': tf.Variable(tf.random_normal([32])),
 	'bc2': tf.Variable(tf.random_normal([64])),
-	'db1': tf.Variable(tf.random_normal([1024])),
+	'bd1': tf.Variable(tf.random_normal([1024])),
 	'out': tf.Variable(tf.random_normal([n_classes]))
-}	
+}
+
+pred = conv_net(x, weights, biases, keep_prob)
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred,y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y,1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+init = tf.initialize_all_variables()
+sess = tf.Session()
+sess.run(init)
+step = 1
+
+while step * batch_size < training_iters:
+	batch_x, batch_y = mnist.train.next_batch(batch_size)
+	sess.run(optimizer, feed_dict={x:batch_x, y:batch_y, keep_prob: dropout})
+	if step % display_step == 0:
+		loss, acc = sess.run([cost, accuracy], feed_dict={x: batch_x, y: batch_y, keep_prob:1.})
+		print "Iter ", str(step* batch_size), "loss ", loss, "Acc ", acc
+	step += 1
+print "Optimization Over"
+
+print "Test Acc", sess.run(accuracy, feed_dict={x: mnist.test.images[:256], y:mnist.test.labels[:256], keep_prob: 1.})
+
