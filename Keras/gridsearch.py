8122eef63081ed8f33b13b5cc294df52c0a95534
krishnakalyan3 <krishnakalyan3@gmail.com>
2017-02-07 16:13:03 +0100

    sklearn

--- Keras/gridsearch.py
+++ Keras/gridsearch.py
@@ -8,9 +8,19 @@ from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
+from keras import backend as K
 
 nb_classes = 10
 img_rows, img_cols = 28,28
 
 (X_train, y_train) , (X_test, y_test) = mnist.load_data()
+
+if K.image_dim_ordering() == 'th':
+	X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
+	X_test = X_test.reshape(X_test.shape[0],1, img_rows, img_cols)
+	input_shape = (1, img_rows, img_cols)
+else:
+	X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 1)
+	X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
+	input_shape = (img_rows, img_cols, 1)
 
