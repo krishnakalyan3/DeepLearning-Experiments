2cfd9c9151274421431d712adb86bdffa14d2141
krishnakalyan3 <krishnakalyan3@gmail.com>
2017-05-05 00:46:00 -0400

    torch vision

---
+++ Torch/0_install_torch.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo pip3 install http://download.pytorch.org/whl/torch-0.1.12.post2-cp36-cp36m-macosx_10_7_x86_64.whl 
+sudo pip3 install torchvision
---
+++ Torch/README.md
@@ -0,0 +1,6 @@
+## PYTORCH Tutorials 
+
+Minimal tutorials for [PyTorch](http://pytorch.org/)
+
+- [Installing Torch]
+- [Tensor Multiplication]
